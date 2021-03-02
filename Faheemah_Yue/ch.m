c7e102ef0230838b11bee9699f6158a255a9f5e8
Yue0925 <60290328+Yue0925@users.noreply.github.com>
2021-03-02 23:24:56 +0100

    CR1

--- Faheemah_Yue/ch.m
+++ Faheemah_Yue/ch.m
@@ -8,22 +8,22 @@ global A B L R S FOUT FIG_XMIN FIG_XMAX FIG_YMIN FIG_YMAX
 
 %% les tests de TP5
 %test5a; FOUT = fopen('test5a.res','w');	
-test5b; FOUT = fopen('test5b.res','w');	
-
+%test5b; FOUT = fopen('test5b.res','w');	
+%test5c; FOUT = fopen('test5c.res','w');
+test5d; FOUT = fopen('test5d.res','w');
 
 lme = [];
 lmi = [];
 
-%% calculer lambda lme initial par moindre carrées
+
+%% calculer lambda initial par moindre carrées
 [e, ce, ci, g, ae, ai, hl, indic] = chs(4, xy, lme, lmi);
 lme = -ae'\g; 
-%% calculer lambda lmi initial
 lmi = -ai'\g; 
 
 options.tol = [1.e-6 1.e-6 1.e-6]; %précision
 options.maxit = 100; %nb max itérations
 options.deriv = 2; % 1: algo Quasi-Newton; 2: méthode de Newton
-
 
 [x, lme, lmi, info] = sqp(@chs, xy, lme, lmi, options);
 info
---
+++ Faheemah_Yue/test5d.m
@@ -0,0 +1,13 @@
+
+%% cas-test 4a
+A = 1;
+B = -1;
+L = [0.8, 0.5, 0.3, 0.4, 0.5]'; % longeur des barres fixées
+
+xy = [0.4   0.6   0.8   1.2   ...
+      0.8   1.4   1   0.8]'; 
+      
+R = [-0.25; -1.5];
+S = [-0.75; 0.4];
+
+FIG_XMIN=-1; FIG_XMAX=2; FIG_YMIN=-1.5; FIG_YMAX=2;
